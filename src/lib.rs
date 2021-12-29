3994658f7816a1536568b4ecd3714c6608afd9ca
Tom Parker-Shemilt <palfrey@tevp.net>
2021-12-29 18:05:34 +0000

    Basic "can parse"

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-use tokio::net::{TcpListener, TcpStream};
-use anyhow::Result;
+use amq_protocol::frame::AMQPFrame;
+use anyhow::{bail, Result};
+use tokio::{
+    io::AsyncReadExt,
+    net::{TcpListener, TcpStream},
+};
 
 pub async fn server() -> Result<()> {
     // Bind the listener to the address
@@ -12,7 +16,77 @@ pub async fn server() -> Result<()> {
         process(socket).await;
     }
 }
+pub struct Connection {
+    stream: TcpStream,
+    buffer: Vec<u8>,
+    cursor: usize,
+}
+
+impl Connection {
+    pub fn new(stream: TcpStream) -> Connection {
+        Connection {
+            stream,
+            buffer: vec![0; 4096],
+            cursor: 0,
+        }
+    }
+
+    pub async fn read_frame(&mut self) -> Result<Option<AMQPFrame>> {
+        loop {
+            match self.parse_frame() {
+                Ok(frame) => {
+                    return Ok(Some(frame));
+                }
+                Err(err) => {
+                    println!("parse error: {}", err);
+                }
+            }
+
+            println!("getting more data at cursor {}", self.cursor);
+
+            // Ensure the buffer has capacity
+            if self.buffer.len() == self.cursor {
+                // Grow the buffer
+                bail!("resize");
+                // println!("resize");
+                // self.buffer.resize(self.cursor * 2, 0);
+            }
+
+            // Read into the buffer, tracking the number
+            // of bytes read
+            let n = self.stream.read(&mut self.buffer[self.cursor..]).await?;
+
+            println!("Got {} bytes", n);
+            if 0 == n {
+                if self.cursor == 0 {
+                    return Ok(None);
+                } else {
+                    bail!("connection reset by peer");
+                }
+            } else {
+                // Update our cursor
+                if self.cursor != 0 {
+                    bail!("foo");
+                }
+                self.cursor += n;
+            }
+        }
+    }
+
+    fn parse_frame(&self) -> Result<AMQPFrame> {
+        return amq_protocol::frame::parsing::parse_frame(&self.buffer[..])
+            .map(|r| r.1)
+            .map_err(|e| e.into());
+    }
+}
 
 async fn process(socket: TcpStream) {
     println!("Got socket {:?}", socket);
+
+    let mut connection = Connection::new(socket);
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("Frame: {:?}", frame);
+    }
+    println!("end process");
 }
--- tests/add_test.rs
+++ tests/add_test.rs
@@ -9,16 +9,18 @@ fn add(x: i32, y: i32) -> TaskResult<i32> {
 
 #[tokio::test]
 async fn test_add() {
-    let handle = tokio::spawn(cavalcade::server());    
-    tokio::time::sleep(Duration::from_secs(1)).await;
+    // let handle = tokio::spawn(cavalcade::server());
+    // tokio::time::sleep(Duration::from_secs(1)).await;
     let my_app = celery::app!(
         broker = AMQPBroker { "amqp://127.0.0.1:5672//" },
         tasks = [add],
         task_routes = [
             "*" => "celery",
         ],
-    ).await.unwrap();
+    )
+    .await
+    .unwrap();
 
     my_app.send_task(add::new(1, 2)).await.unwrap();
-    handle.await.unwrap().unwrap();
+    // handle.await.unwrap().unwrap();
 }
