40d2b4359ddcaf616e7f295c5caa5c83957449dc
Tom Parker-Shemilt <palfrey@tevp.net>
2022-01-01 13:25:45 +0000

    Fix Python formatting

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -7,23 +7,33 @@ import os
 AMQP_HOST = os.environ.get("AMQP_HOST", "localhost")
 print("host", AMQP_HOST)
 
-app = Celery('hello', broker=f"amqp://guest@{AMQP_HOST}//", config_source={'broker_connection_retry': False, 'broker_transport_options': {'max_retries': 0}})
+app = Celery(
+    "hello",
+    broker=f"amqp://guest@{AMQP_HOST}//",
+    config_source={
+        "broker_connection_retry": False,
+        "broker_transport_options": {"max_retries": 0},
+    },
+)
+
 
 @app.task
 def hello():
-    return 'hello world'
+    return "hello world"
+
 
 class CeleryThread(threading.Thread):
     def run(self):
         app.start(sys.argv[1:])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     ct = CeleryThread()
     ct.setDaemon(True)
     ct.start()
 
     print("hello", hello.delay())
-    
+
     time.sleep(5)
 
     assert ct.is_alive()
