643219be7a9cca4f16ba063302a1de737edb31ec
Tom Parker-Shemilt <palfrey@tevp.net>
2022-01-06 11:48:34 +0000

    Actually get the current pr

--- .github/no-annotations.js
+++ .github/no-annotations.js
@@ -1,18 +1,16 @@
+const fs = require('fs')
+
 module.exports = async ({github, context, core}) => {
-  const {SHA} = process.env
-  const commit = await github.rest.repos.getCommit({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    ref: `${SHA}`
-  })
+  const {PR_NUM, REPO_OWNER} = process.env;
     const { data: pullRequest } = await github.rest.pulls.get({
-        owner: 'palfrey',
+        owner: REPO_OWNER,
         repo: 'cavalcade',
-        pull_number: 123,
+        pull_number: PR_NUM,
         mediaType: {
           format: 'diff'
         }
     });
 
     console.log(pullRequest);
+    throw "foo";
 }
