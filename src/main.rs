96d36877d27b08e9dd4bbd8a7e9720fb733582f4
Tom Parker-Shemilt <palfrey@tevp.net>
2022-01-16 20:49:53 +0000

    Add production build that can actually do migrate

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,57 @@
-use clap::App;
+use std::process::{exit, Command};
+
+use clap::{App, Arg};
 
 #[tokio::main]
 async fn main() {
-    App::new("cavalcade")
+    let matches = App::new("cavalcade")
         .version(clap::crate_version!())
+        .arg(
+            Arg::new("migrate")
+                .long("migrate")
+                .help("Run sqlx migrations"),
+        )
+        .arg(
+            Arg::new("sqlx-path")
+                .long("sqlx-path")
+                .help("Path to sqlx")
+                .default_value("sqlx")
+                .takes_value(true),
+        )
         .get_matches();
+
+    if matches.is_present("migrate") {
+        let sqlx_path = matches.value_of("sqlx-path").unwrap();
+        if !Command::new(sqlx_path)
+            .args(["migrate", "info"])
+            .spawn()
+            .expect(&format!(
+                "Expected sqlx spawn to work for path '{}'",
+                sqlx_path
+            ))
+            .wait()
+            .expect("Migration failure")
+            .success()
+        {
+            exit(-1);
+        }
+
+        if !Command::new(sqlx_path)
+            .args(["migrate", "run"])
+            .spawn()
+            .expect(&format!(
+                "Expected sqlx spawn to work for path '{}'",
+                sqlx_path
+            ))
+            .wait()
+            .expect("Migration failure")
+            .success()
+        {
+            exit(-1);
+        }
+        return;
+    }
+
     match log4rs::init_file("log4rs.yml", Default::default()) {
         Ok(_) => {}
         Err(err) => {
