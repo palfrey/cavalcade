f80c6fcea825919a0c6503e8b7a9dde8eb344fb6
Tom Parker-Shemilt <palfrey@tevp.net>
2022-01-16 17:38:54 +0000

    Do static build test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+use clap::App;
+
 #[tokio::main]
 async fn main() {
+    App::new("cavalcade")
+        .version(clap::crate_version!())
+        .get_matches();
     match log4rs::init_file("log4rs.yml", Default::default()) {
         Ok(_) => {}
         Err(err) => {
